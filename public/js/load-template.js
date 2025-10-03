7072acaae47295513aa0e79803ea768b429ae754
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-10-03 10:30:44 +0100

    a

--- public/js/load-template.js
+++ public/js/load-template.js
@@ -6,12 +6,15 @@ function displayTemplate(html, divSelector) {
     div.remove();
 }
 
-const fetchHeader = fetch(`${templateFolder}header.html`);
-const fetchFooter = fetch(`${templateFolder}footer.html`);
+function renderTemplate() {
+    const fetchHeader = fetch(`${templateFolder}header.html`);
+    const fetchFooter = fetch(`${templateFolder}footer.html`);
+    Promise.all([fetchHeader, fetchFooter])
+        .then(values => Promise.all(values.map(r => r.text())))
+        .then(([header, footer]) => {
+            displayTemplate(header, '#header');
+            displayTemplate(footer, '#footer');
+        }).catch(err => console.error('There was an error rendering template', err))
+}
 
-Promise.all([fetchHeader, fetchFooter])
-.then(values => Promise.all(values.map(r => r.text())) )
-.then(([header, footer]) =>{
-    displayTemplate(header, '#header');
-    displayTemplate(footer,'#footer');
-}).catch(err=> console.error('There was an error rendering template', err))
+renderTemplate()
