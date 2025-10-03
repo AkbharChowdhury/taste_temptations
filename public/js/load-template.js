94142ff6fbc335075781d1921999038d26f2f686
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-10-03 13:16:40 +0100

    a

--- public/js/load-template.js
+++ public/js/load-template.js
@@ -6,15 +6,31 @@ function displayTemplate(html, divSelector) {
     div.remove();
 }
 
-function renderTemplate() {
-    const fetchHeader = fetch(`${templateFolder}header.html`);
-    const fetchFooter = fetch(`${templateFolder}footer.html`);
-    Promise.all([fetchHeader, fetchFooter])
-        .then(values => Promise.all(values.map(r => r.text())))
-        .then(([header, footer]) => {
-            displayTemplate(header, '#header');
-            displayTemplate(footer, '#footer');
-        }).catch(err => console.error('There was an error rendering template', err))
-}
+// function renderTemplate() {
+//     const fetchHeader = fetch(`${templateFolder}header.html`);
+//     const fetchFooter = fetch(`${templateFolder}footer.html`);
+//     Promise.all([fetchHeader, fetchFooter])
+//         .then(values => Promise.all(values.map(r => r.text())))
+//         .then(([header, footer]) => {
+//             displayTemplate(header, '#header');
+//             displayTemplate(footer, '#footer');
+//         }).catch(err => console.error('There was an error rendering template', err))
+// }
 
-renderTemplate()
+// renderTemplate()
+
+const renderTemplate = async () => {
+    try {
+        const data = await Promise.all([fetch(`${templateFolder}header.html`), fetch(`${templateFolder}footer.html`)]);
+        const [header, footer] = await Promise.all(data.map(r => r.text()));
+        return { header, footer }
+    } catch (err) {
+        console.error('There was an error rendering template', err)
+    }
+}
+renderTemplate().then(data => {
+    const { header, footer } = data;
+    displayTemplate(header, '#header');
+    displayTemplate(footer, '#footer');
+
+});
