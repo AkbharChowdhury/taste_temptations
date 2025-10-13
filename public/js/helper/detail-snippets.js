bcb56d9ba5e629d865808da2e2c19045b20fb0b9
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-10-13 20:00:21 +0100

    a

--- public/js/helper/detail-snippets.js
+++ public/js/helper/detail-snippets.js
@@ -12,4 +12,23 @@ export const getIngredientsList = ({ extendedIngredients }) =>
         ${getListItem(extendedIngredients, 'original')}
     </ul>   
     `;
+export function showExtraInfo({ vegan, vegetarian, glutenFree, diets }) {
+    const showTag = (type, value) =>  value ? /*html*/`
+                <span class="badge text-bg-success">${titleCase(type)}</span>
+            ` : '';
+    
+    const tags = [
+        showTag('vegan', vegan),
+        showTag('vegetarian', vegetarian),
+        showTag('gluten free', glutenFree)
+    ];
 
+    const container = document.querySelector('#tags-data');
+    const availableTags = tags.filter(i => i).join();
+    const dietText =`<p class="pt-2">Suitable for diets: <strong>${diets}</strong></p>` ;
+
+    container.insertAdjacentHTML('beforebegin', availableTags.replaceAll(',',''));
+    container.insertAdjacentHTML('beforebegin', dietText);
+
+
+}
