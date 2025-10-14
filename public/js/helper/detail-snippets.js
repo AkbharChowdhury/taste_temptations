7b45cd18a560d715f09a2046761e2b98a19c65fc
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-10-14 09:54:30 +0100

    a

--- public/js/helper/detail-snippets.js
+++ public/js/helper/detail-snippets.js
@@ -1,6 +1,6 @@
 
 import { titleCase } from './utils.js';
- 
+
 const getListItem = (items, key) => items.map(item => /*html*/`<li>${item[key]}</li>`).join().replaceAll(',', '');
 
 export const getSteps = ({ steps }) =>
@@ -15,22 +15,17 @@ export const getIngredientsList = ({ extendedIngredients }) =>
     </ul>   
     `;
 export function showExtraInfo({ vegan, vegetarian, glutenFree, diets }) {
-    const showTag = (type, value) =>  value ? /*html*/`
-                <span class="badge text-bg-success">${titleCase(type)}</span>
-            ` : '';
-    
-    const tags = [
-        showTag('vegan', vegan),
-        showTag('vegetarian', vegetarian),
-        showTag('gluten free', glutenFree)
-    ];
-
+    const showTag = (type, value) => value ? /*html*/`<span class="badge text-bg-success">${titleCase(type)}</span>` : '';
+    const tags = [showTag('vegan', vegan), showTag('vegetarian', vegetarian), showTag('gluten free', glutenFree)];
     const container = document.querySelector('#tags-data');
+    const hasDiet = diets.length > 0;
     const availableTags = tags.filter(i => i).join();
-    const dietText =`<p class="pt-2">Suitable for diets: <strong>${diets}</strong></p>` ;
-
-    container.insertAdjacentHTML('beforebegin', availableTags.replaceAll(',',''));
-    container.insertAdjacentHTML('beforebegin', dietText);
-
+    const dietText = `<p class="pt-2">Suitable for diets: <strong>${diets}</strong></p>`;
+    container.insertAdjacentHTML('beforebegin', availableTags.replaceAll(',', ''));
+    if (hasDiet) container.insertAdjacentHTML('beforebegin', dietText);
+    const isContainerEmpty = availableTags.length === 0 && !hasDiet;
+    if (isContainerEmpty) document.querySelector('#tags').remove();
 
 }
+
+
