673018aefaf09085b4b2afb5f8519c3c88a256c4
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2026-04-08 19:19:31 +0100

    a

--- recipe.js
+++ recipe.js
@@ -10,60 +10,67 @@ const API_KEY = process.env.API_KEY;
 const BASE_URL = 'https://api.spoonacular.com/recipes/';
 const RECORDS_PER_PAGE = 6;
 
-axios.defaults.headers['x-api-key'] = API_KEY;
+axios.defaults.headers.common['x-api-key'] = API_KEY;
 axios.defaults.baseURL = BASE_URL;
-
-const requestData = (url, contentType = 'application/json') => new Request(url, { headers: { 'x-api-key': API_KEY, 'Content-Type': contentType } });
 
 export class Recipe {
     #recipeUI;
     constructor() {
-        if (!!Recipe.instance) return Recipe.instance;
+        if (Recipe.instance) return Recipe.instance;
         Recipe.instance = this;
         this.#recipeUI = new RecipeUI(RECORDS_PER_PAGE);
-        return this;
     }
 
-    get recipeUI (){ return this.#recipeUI }
+    get recipeUI() {
+        return this.#recipeUI;
+    }
 
-    async #request(url, params = new URLSearchParams()) {
-        const response = await axios.get(url, { params });
+    async #request(url, { params = new URLSearchParams(), headers = {} } = {}) {
+        const response = await axios.get(url, {
+            params,
+            headers,
+        });
         return response.data;
     }
 
     async search(urlSearchParams) {
-        const params = new URLSearchParams(urlSearchParams);
-        params.append('number', params.get('number') ?? RECORDS_PER_PAGE);
-        params.append('addRecipeInformation', true);
-        params.append('sort', 'random');
-        return this.#request('complexSearch', params)
-    }
+        const baseParams = Object.fromEntries(urlSearchParams);
+        const additionalParams = {
+            number: urlSearchParams.get('number') ?? RECORDS_PER_PAGE,
+            addRecipeInformation: true,
+            sort: 'random',
+        };
 
+        const params = new URLSearchParams({
+            ...baseParams,
+            ...additionalParams,
+        });
+
+        return this.#request('complexSearch', { params });
+    }
 
     async similar(id) {
-        const number = 8;
-        const params = new URLSearchParams({ number });
-        return this.#request(`${id}/similar`, params);
+        const params = new URLSearchParams({ number: 8 });
+        return this.#request(`${id}/similar`, { params });
     }
 
-    details = async id => this.#request(`${id}/information`);
+    async details(id) {
+        return this.#request(`${id}/information`);
+    }
 
     async random() {
         const randomCuisine = getRandomItem(cuisines);
         const randomMeal = getRandomItem(mealTypes);
         const tags = [randomMeal, randomCuisine];
-        const number = RECORDS_PER_PAGE;
         const params = new URLSearchParams({
-            number,
+            number: RECORDS_PER_PAGE,
             'include-tags': tags.join()
         });
-        return this.#request('random', params);
-
+        return this.#request('random', { params });
     }
 
     async nutritionLabelWidget(id) {
-        const headers = { 'Content-Type': 'text/html' };
-        const response = await fetch(requestData(`${BASE_URL}${id}/nutritionLabel`, headers['Content-Type']));
-        return await response.text();
+        const headers = { Accept: 'text/html' }
+        return this.#request(`${id}/nutritionLabel`, { headers });
     }
 }
