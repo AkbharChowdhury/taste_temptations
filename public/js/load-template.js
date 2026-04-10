bbc4ac85d066199f183bb122de3646dd55684213
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2026-04-10 19:41:12 +0100

    a

--- public/js/load-template.js
+++ public/js/load-template.js
@@ -1,44 +1,28 @@
-// function displayTemplate(html, divSelector) {
-//     const div = document.querySelector(divSelector);
-//     div.insertAdjacentHTML("beforebegin", html)
-//     div.remove();
-// }
-
-// async function renderTemplate() {
-//     try {
-//         const templateFolder = 'template';
-//         const files = ['header.html', 'footer.html'];
-//         const data = await Promise.all(files.map(file => fetch(`${templateFolder}/${file}`)));
-//         const [header, footer] = await Promise.all(data.map(r => r.text()));
-//         return { header, footer }
-//     } catch (err) {
-//         console.log('There was an error rendering template', err);
-//     }
-
-// }
-
-// renderTemplate().then(({ header, footer }) => {
-//     displayTemplate(header, '#header');
-//     displayTemplate(footer, '#footer');
-//     document.querySelector('#load-template')?.remove();
-// });
+(async () => {
+    const templates = await loadTemplates();
+    renderTemplates(templates);
+    document.querySelector('#load-template')?.remove();
+})();
 
 async function loadTemplates() {
-
     const templateFolder = 'template';
-    const templates = [
+
+    const templateConfigs = [
         { selector: '#header', file: 'header.html' },
-        { selector: '#footer', file: 'footer.html' }
+        { selector: '#footer', file: 'footer.html' },
     ];
 
     try {
-        const responses = await Promise.all(templates.map(t => fetch(`${templateFolder}/${t.file}`)));
-        const htmlTexts = await Promise.all(responses.map(res => res.text()) );
+        const templates = await Promise.all(
 
-        return templates.map((t, index) => ({
-            selector: t.selector,
-            html: htmlTexts[index]
-        }));
+            templateConfigs.map(async ({ selector, file }) => {
+                const response = await fetch(`${templateFolder}/${file}`);
+                const html = await response.text();
+                return { selector, html };
+            })
+        );
+
+        return templates;
 
     } catch (err) {
         console.error('Error loading templates:', err);
@@ -48,17 +32,9 @@ async function loadTemplates() {
 
 function renderTemplates(templatesData) {
     templatesData.forEach(({ selector, html }) => {
-        const div = document.querySelector(selector);
-        if(!div) return;
-        div.insertAdjacentHTML("beforebegin", html);
-        div.remove();
+        const el = document.querySelector(selector);
+        if (!el) return;
+        el.insertAdjacentHTML("beforebegin", html);
+        el.remove();
     });
 }
-loadTemplates()
-.then(renderTemplates)
-.finally(() => document.querySelector('#load-template')?.remove());
-
-
-
-
-
