eed27fdeca7d44f40dfe7b56734e4a1545945c95
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2026-06-01 16:37:19 +0100

    a

--- public/js/helper/recipe-card.js
+++ public/js/helper/recipe-card.js
@@ -7,7 +7,7 @@ const renderCheapBadge = isCheap => `<i class="fa-solid fa-${isCheap ? 'tag' : '
 
 const recipeDetailURL = id => `detail.html?recipeID=${id}`;
 const getRecipeImage = id => `https://img.spoonacular.com/recipes/${id}-556x370.jpg`;
-const getDataTag = tagName => `[data-${tagName}]`;
+const getDataAttribute = name => `[data-${name}]`;
 
 
 function renderDairyIcon(badgeCol = 'danger', text = 'Contains Dairy') {
@@ -26,32 +26,32 @@ export const recipeCard = (recipe, renderContext) => {
     cheap,
     dairyFree,
   } = recipe;
-    const clone = getTemplateClone(templateSelector);
+  const clone = getTemplateClone(templateSelector);
 
   const badges = {
-    glutenFree: clone.querySelector(getDataTag('gluten-free')), 
-    dairyFree: clone.querySelector(getDataTag('dairy-free')),
-    popular: clone.querySelector(getDataTag('popular')),
-    cheap: clone.querySelector(getDataTag('is-cheap')),
+    glutenFree: clone.querySelector(getDataAttribute('gluten-free')),
+    dairyFree: clone.querySelector(getDataAttribute('dairy-free')),
+    popular: clone.querySelector(getDataAttribute('popular')),
+    cheap: clone.querySelector(getDataAttribute('is-cheap')),
   };
 
-  
+
   const img = clone.querySelector('img');
   img.src = image;
   img.alt = title;
 
 
   clone.querySelector('.card-title').textContent = title;
-  clone.querySelector(getDataTag('servings')).innerText = servings;
-  clone.querySelector(getDataTag('likes')).innerText = likes.toLocaleString();
-  clone.querySelector(getDataTag('duration')).textContent = formatDuration(minutes, DurationFormatStyle.NARROW);
+  clone.querySelector(getDataAttribute('servings')).innerText = servings;
+  clone.querySelector(getDataAttribute('likes')).innerText = likes.toLocaleString();
+  clone.querySelector(getDataAttribute('duration')).textContent = formatDuration(minutes, DurationFormatStyle.NARROW);
   clone.querySelector('.card-link').setAttribute('href', recipeDetailURL(id));
-  clone.querySelector(getDataTag('weightWatcherSmartPoints')).innerText = healthPoints;
+  clone.querySelector(getDataAttribute('weightWatcherSmartPoints')).innerText = healthPoints;
 
   clone.querySelector(healthProgressSelector).setAttribute('value', healthScore);
 
   // ─── BADGES ───────────────────
-  glutenFree &&  badges.glutenFree.classList.replace('text-bg-danger', 'text-bg-success');
+  glutenFree && badges.glutenFree.classList.replace('text-bg-danger', 'text-bg-success');
   !veryPopular && badges.popular.remove();
   badges.cheap.innerHTML = renderCheapBadge(cheap);
   badges.dairyFree.innerHTML = !dairyFree ? renderDairyIcon() : renderDairyIcon('success', 'dairy free');
@@ -69,11 +69,11 @@ export const similarRecipeCard = (recipe, renderContext) => {
   img.src = getRecipeImage(id);
   img.alt = title;
   img.title = title;
-
-  clone.querySelector(getDataTag('title')).textContent = title;
-  clone.querySelector('a').setAttribute('href', recipeDetailURL(id));
-  clone.querySelector(getDataTag('duration')).textContent = formatDuration(minutes, DurationFormatStyle.SHORT);
-  clone.querySelector('h3').textContent = `serves ${servings}`;
+  
+  clone.querySelector(getDataAttribute('servings')).textContent = `serves ${servings}`;
+  clone.querySelector(getDataAttribute('title')).textContent = title;
+  clone.querySelector(getDataAttribute('duration')).textContent = formatDuration(minutes, DurationFormatStyle.SHORT);
+  clone.querySelector('a').href = recipeDetailURL(id);
 
   return clone;
 
